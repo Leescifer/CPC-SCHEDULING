37687b23580eaa4e0884c284e18e0a1505e36379
Leecifer <Leester9103@gmail.com>
2024-12-03 22:46:53 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ter = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+        ter.close();
+        
+    }
+}
